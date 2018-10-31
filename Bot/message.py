46783ed1cc43560b3120b346b4f537dd70964609
Sami <sami.bosch@gmail.com>
2018-10-31 18:13:34 +0100

    simple command detection

--- Bot/message.py
+++ Bot/message.py
@@ -7,5 +7,14 @@ import discord
 
 async def respond(message, client):
     if message.author != client.user:
-        await client.send_message(message.author, "hoi scrub")
-        await client.send_message(message.channel, "hoi there")
+        content = message.content
+        if content.startswith('!'):
+            print(content)
+            if content.find(" ") > 0:
+                command = content[1:content.find(" ") + 1]
+            else:
+                command = content[1:]
+            print(command)
+            if command == "ping":
+                await client.send_message(message.author, "pong")
+                await client.send_message(message.channel, "pong")
