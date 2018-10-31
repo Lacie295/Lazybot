0a7d09b066385c69fded9f612498964df02fbede
Sami <sami.bosch@gmail.com>
2018-10-31 15:39:33 +0100

    Move config, test and __init__ files ready

---
+++ Bot/__init__.py
@@ -0,0 +1,11 @@
+# created by Sami Bosch on Wednesday, 31 October 2018
+
+# This script launches the necessary functions for the real bot
+
+import json
+from run import runbot
+
+
+with open('config.json') as f:
+    data = json.load(f)
+runbot(data["Token"])
---
+++ Bot/run.py
@@ -0,0 +1,9 @@
+# created by Sami Bosch on Wednesday, 31 October 2018
+
+# This file contains all functions necessary to start up the bot
+
+# import discord
+
+
+def runbot(token):
+    pass
