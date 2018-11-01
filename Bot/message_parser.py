6e38c95e258d51fb556ac480b6f9c1825f921aba
Sami <sami.bosch@gmail.com>
2018-11-01 11:59:50 +0100

    Muting

--- Bot/message_parser.py
+++ Bot/message_parser.py
@@ -25,7 +25,7 @@ def init(client):
     @client.command(pass_context=True)
     async def ban(context):
         """Takes a list of mentioned users + optionally an int. Bans all users in list, and if int has been supplied,
-        unbans them after given time."""
+        unbans them after given time in days."""
         m = context.message
         if m.content.find(" ") > 0:
             try:
@@ -38,13 +38,14 @@ def init(client):
         if m.author.server_permissions.ban_members:
             for member in m.mentions:
                 await client.ban(member, delete_message_days=0)
-                await client.say("banned {}".format(member.name))
+                await client.say("banned {} for {} days (-1 = indefinite)".format(member.name, unban_time))
+
             if unban_time >= 0:
                 async def unban_all():
-                    for mem in m.mentions:
+                    for member in m.mentions:
                         await client.unban(m.server, member)
                         await client.send_message(m.channel, "unbanned {}".format(member.name))
-                t = AsyncTimer(unban_time, unban_all)
+                t = AsyncTimer(unban_time * 86400, unban_all)
         else:
             await client.say("you do not have the permission to ban users")
 
@@ -58,3 +59,33 @@ def init(client):
                 await client.say("kicked {}".format(member.name))
         else:
             await client.say("you do not have the permission to kick users")
+
+    @client.command(aliases=['mute', 'silence'], pass_context=True)
+    async def timeout(context):
+        """Takes a list of mentioned users and a timeout at the end of the message and silences all users for the
+        specified time in minutes."""
+        m = context.message
+        muted = discord.utils.get(m.server.roles, name='Muted')
+        if m.content.find(" ") > 0:
+            try:
+                mute_time = float(m.content.split(" ")[-1])
+            except ValueError:
+                mute_time = -1
+        else:
+            mute_time = -1
+
+        if m.author.server_permissions.manage_roles and mute_time >= 0:
+            for member in m.mentions:
+                await client.add_roles(member, muted)
+                await client.say("Muted {} for {} minutes".format(member.name, int(mute_time)))
+            if mute_time >= 0:
+                async def unban_all():
+                    for member in m.mentions:
+                        await client.remove_roles(member, muted)
+                        await client.send_message(m.channel, "Unmuted {}".format(member.name))
+
+                AsyncTimer(mute_time * 60, unban_all)
+        elif mute_time == -1:
+            await client.say("Please provide a time (in minutes)")
+        else:
+            await client.say("you do not have the permission to ban users")
