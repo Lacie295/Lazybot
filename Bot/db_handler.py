d85854627517c785f9f72b45e8238ddf5316c9db
Sami <sami.bosch@gmail.com>
2018-11-02 15:21:49 +0100

    db skeleton

---
+++ Bot/db_handler.py
@@ -0,0 +1,23 @@
+# created by Sami Bosch on Friday, 2 November 2018
+
+# This class handles all accesses to db
+
+
+def __init__():
+    pass
+
+
+def add_song_to_queue(url, author, comment, date):
+    pass
+
+
+def get_song():
+    pass
+
+
+def store_history(history):
+    pass
+
+
+def get_history():
+    pass
