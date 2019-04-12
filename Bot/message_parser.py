a442fefc90df219440246de7cfc345294eca860b
Sami <sami.bosch@gmail.com>
2019-04-12 16:02:17 +0200

    Rights check before syntax check

--- Bot/message_parser.py
+++ Bot/message_parser.py
@@ -41,15 +41,16 @@ def init(client):
         """Takes a list of mentioned users + optionally an int. Bans all users in list, and if int has been supplied,
         unbans them after given time in days."""
         m = context.message
-        if m.content.find(" ") > 0:
-            try:
-                unban_time = float(m.content.split(" ")[-1])
-            except ValueError:
-                unban_time = -1
-        else:
-            unban_time = -1
 
         if m.author.server_permissions.ban_members:
+            if m.content.find(" ") > 0:
+                try:
+                    unban_time = float(m.content.split(" ")[-1])
+                except ValueError:
+                    unban_time = -1
+            else:
+                unban_time = -1
+
             for member in m.mentions:
                 await client.ban(member, delete_message_days=0)
                 await client.say("Banned {} for {} days (-1 = indefinite).".format(member.name, unban_time))
@@ -80,28 +81,30 @@ def init(client):
         """Takes a list of mentioned users and a timeout at the end of the message and silences all users for the
         specified time in minutes."""
         m = context.message
-        muted = discord.utils.get(m.server.roles, name='Muted')
-        if m.content.find(" ") > 0:
-            try:
-                mute_time = float(m.content.split(" ")[-1])
-            except ValueError:
-                mute_time = -1
-        else:
-            mute_time = -1
 
-        if m.author.server_permissions.manage_roles and mute_time >= 0:
-            for member in m.mentions:
-                await client.add_roles(member, muted)
-                await client.say("Muted {} for {} minutes.".format(member.name, int(mute_time)))
-            if mute_time >= 0:
-                async def unban_all():
+        if m.author.server_permissions.manage_roles:
+            muted = discord.utils.get(m.server.roles, name='Muted')
+            if m.content.find(" ") > 0:
+                try:
+                    mute_time = float(m.content.split(" ")[-1])
+                except ValueError:
+                    mute_time = -1
+            else:
+                mute_time = -1
+
+            if mute_time > 0:
+                for member in m.mentions:
+                    await client.add_roles(member, muted)
+                    await client.say("Muted {} for {} minutes.".format(member.name, int(mute_time)))
+
+                async def unmute_all():
                     for member in m.mentions:
                         await client.remove_roles(member, muted)
                         await client.send_message(m.channel, "Unmuted {}.".format(member.name))
 
-                AsyncTimer(mute_time * 60, unban_all)
-        elif mute_time == -1:
-            await client.say("Please provide a time (in minutes).")
+                AsyncTimer(mute_time * 60, unmute_all)
+            else:
+                await client.say("Please provide a time (in minutes).")
         else:
             await client.say("You do not have the permission to ban users.")
 
